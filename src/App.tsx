db098794981ec198a83b8a20e93054fd8d4b0339
scf256 <scf437617@gmail.com>
2026-05-23 16:10:46 +0300

    Add basename to BrowserRouter for GitHub Pages subdirectory

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/cfs">
           <ScrollToTop />
           <Layout>
           <Routes>
